67f00f8c38fe34afad623f050fcd6dcd3c5f32b4
Anthony Weathersby <anthonycweathersby@gmail.com>
2018-12-20 21:26:17 -0700

    Added .npmignore

--- build/css.node.js
+++ build/css.node.js
@@ -1562,9 +1562,6 @@ class CSS_Color extends Color {
                     out.b = parseInt(l.next().tx);
                     l.next(); // ,
                     out.a = parseFloat(l.next().tx);
-                    l.next().next();
-                    c = new CSS_Color();
-                    c.set(out);
                     break;
                 } else if (tx == "rgb") {
                     out = { r: 0, g: 0, b: 0, a: 1 };
@@ -1575,8 +1572,10 @@ class CSS_Color extends Color {
                     l.next(); // ,
                     out.b = parseInt(l.next().tx);
                     l.next();
-                    break;
                 }
+                c = new CSS_Color();
+                c.set(out);
+                return c;
             default:
                 let string = l.tx;
 
@@ -4074,85 +4073,87 @@ class CSS_Bezier extends CBezier {
 }
 
 class Stop{
-	constructor(color, percentage){
-		this.color = color;
-		this.percentage = percentage || null;
-	}
+    constructor(color, percentage){
+        this.color = color;
+        this.percentage = percentage || null;
+    }
 
-	toString(){
-		return `${this.color}${(this.percentage)?" "+this.percentage:""}`;
-	}
+    toString(){
+        return `${this.color}${(this.percentage)?" "+this.percentage:""}`;
+    }
 }
 
 class CSS_Gradient{
 
-	static parse(l, rule, r) {
+    static parse(l, rule, r) {
         let tx = l.tx,
             pky = l.pk.ty;
         if (l.ty == l.types.id) {
-        	switch(l.tx){
-        		case "linear-gradient":
-        		l.next().a("(");
-        		let dir,num,type ="deg";
-        		if(l.tx == "to"){
+            switch(l.tx){
+                case "linear-gradient":
+                l.next().a("(");
+                let dir,num,type ="deg";
+                if(l.tx == "to"){
 
-        		}else if(l.ty == l.types.num){
-        			num = parseFloat(l.ty);
-        			type = l.next().tx;
-        			l.next().a(',');
-        		}
-        		let stops = [];
-        		while(!l.END && l.ch != ")"){
-        			let v = CSS_Color.parse(l, rule, r);
-        			let len = null;
+                }else if(l.ty == l.types.num){
+                    num = parseFloat(l.ty);
+                    type = l.next().tx;
+                    l.next().a(',');
+                }
 
-        			if(l.ch == ")") {
-        				stops.push(new Stop(v, len));
-        				break;
-        			}
-        			
-        			if(l.ch != ","){
-	        			if(!(len = CSS_Length.parse(l, rule, r)))
-	        				len = CSS_Percentage.parse(l,rule,r);
-        			}else{
-        				l.next();
-        			}
+                let stops = [];
+                
+                while(!l.END && l.ch != ")"){
+                    let v = CSS_Color.parse(l, rule, r);
+                    let len = null;
 
-        			stops.push(new Stop(v, len));
-        		}
-        		l.a(")");
-        		let grad = new CSS_Gradient();
-        		grad.stops = stops;
-        		return grad;
-        	}
+                    if(l.ch == ")") {
+                        stops.push(new Stop(v, len));
+                        break;
+                    }
+                    
+                    if(l.ch != ","){
+                        if(!(len = CSS_Length.parse(l, rule, r)))
+                            len = CSS_Percentage.parse(l,rule,r);
+                    }else
+                        l.next();
+                    
+
+                    stops.push(new Stop(v, len));
+                }
+                l.a(")");
+                let grad = new CSS_Gradient();
+                grad.stops = stops;
+                return grad;
+            }
         }
         return null;
     }
 
 
-	constructor(type = 0){
-		this.type = type; //linear gradient
-		this.direction = new CSS_Length(0, "deg");
-		this.stops = [];
-	}
+    constructor(type = 0){
+        this.type = type; //linear gradient
+        this.direction = new CSS_Length(0, "deg");
+        this.stops = [];
+    }
 
-	toString(){
-		let str = [];
-		switch(this.type){
-			case 0:
-			str.push("linear-gradient(");
-			if(this.direction !== 0)
-				str.push(this.direction.toString() + ",");
-			break;
-		}
+    toString(){
+        let str = [];
+        switch(this.type){
+            case 0:
+            str.push("linear-gradient(");
+            if(this.direction !== 0)
+                str.push(this.direction.toString() + ",");
+            break;
+        }
 
-		for(let i = 0; i < this.stops.length; i++)
-			str.push(this.stops[i].toString()+((i<this.stops.length-1)?",":""));
+        for(let i = 0; i < this.stops.length; i++)
+            str.push(this.stops[i].toString()+((i<this.stops.length-1)?",":""));
 
-		str.push(")");
+        str.push(")");
 
-		return str.join(" ");
-	}
+        return str.join(" ");
+    }
 }
 
 const $medh = (prefix) => ({
