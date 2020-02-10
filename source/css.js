3a8390c0c1b366c1ecc6f3b970d8446dadff05f6
Anthony <anthonycweathersby@gmail.com>
2020-02-10 11:08:04 -0700

    removed duplicate import statement

--- source/css.js
+++ source/css.js
@@ -1,6 +1,6 @@
 import whind from "@candlefw/whind";
 import css_parser from "./Parser/css.js";
-import { types, property_definitions, media_feature_definitions } from "./properties/property_and_type_definitions.js";
+import { property_definitions, media_feature_definitions } from "./properties/property_and_type_definitions.js";
 import { getPropertyParser } from "./properties/parser.js";
 import * as productions from "./properties/productions.js";
 import * as terms from "./properties/terms.js";
@@ -18,8 +18,42 @@ import attribSelector from "./selectors/attribute.js"
 import pseudoClassSelector from "./selectors/pseudo_class.js"
 import pseudoElementSelector from "./selectors/pseudo_element.js"
 import parseDeclaration from "./properties/style_prop_parse_declaration.js"
+
 import CSS_Length from "./types/length.js";
 import CSS_URL from "./types/url.js";
+import CSS_Color from "./types/color.js";
+import CSS_Percentage from "./types/percentage.js";
+import CSS_String from "./types/string.js";
+import CSS_Id from "./types/id.js";
+import CSS_Shape from "./types/shape.js";
+import CSS_Number from "./types/number.js";
+import CSS_Bezier from "./types/cubic_bezier.js";
+import CSS_Gradient from "./types/gradient.js";
+import CSS_Media_handle from "./types/media.js";
+import CSS_Transform2D from "./types/transform.js";
+import CSS_Path from "./types/path.js";
+import CSS_FontName from "./types/font_name.js"
+
+const types = {color: CSS_Color,
+    length: CSS_Length,
+    time: CSS_Length,
+    flex: CSS_Length,
+    angle: CSS_Length,
+    frequency: CSS_Length,
+    resolution: CSS_Length,
+    percentage: CSS_Percentage,
+    url: CSS_URL,
+    uri: CSS_URL,
+    number: CSS_Number,
+    id: CSS_Id,
+    string: CSS_String,
+    shape: CSS_Shape,
+    cubic_bezier: CSS_Bezier,
+    integer: CSS_Number,
+    gradient: CSS_Gradient,
+    transform2D : CSS_Transform2D,
+    path: CSS_Path,
+    fontname: CSS_FontName}
 
 const env = {
     functions: {
