5125e47a78aa0c30891d8742da225f7afc434a9f
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-08-28 15:12:26 -0600

    Add parseProperty function

--- source/typescript/properties/parse_declaration.ts
+++ source/typescript/properties/parse_declaration.ts
@@ -1,10 +1,10 @@
-import observer from "@candlefw/observer";
 import wind from "@candlefw/wind";
 import {
     property_definitions
 } from "./property_and_type_definitions.js";
 import { getPropertyParser } from "./parser.js";
 import { CSSProperty } from "./property.js";
+import { CSS_String } from "../css.js";
 
 /* 
     Parses a string value of a css property. Returns result of parse or null.
@@ -21,9 +21,7 @@ import { CSSProperty } from "./property.js";
         important : boolean value indicating the presence of "important" value.
 */
 
-observer("updatedCSSStyleProperty", CSSProperty.prototype);
-
-export default function parseDeclaration(sym, a, b, pos) {
+export default function parsePropertyDefinitionFromHydrocarbon(sym: { 0: string, 2: string, 3: boolean; length: number; }, a, b, pos): CSSProperty {
 
 
     if (sym.length == 0)
@@ -31,18 +29,24 @@ export default function parseDeclaration(sym, a, b, pos) {
 
     let prop = null;
 
+
     const
         rule_name = sym[0],
-        body_string = sym[2],
+        body_string = sym[2].trim(),
         important = sym[3] ? true : false,
-        IS_VIRTUAL = { is: false },
-        parser = getPropertyParser(rule_name.replace(/\-/g, "_"), IS_VIRTUAL, property_definitions);
+        IS_VIRTUAL = { is: false };
 
-    if (parser && !IS_VIRTUAL.is)
+
+    const parser = getPropertyParser(rule_name.replace(/\-/g, "_"), IS_VIRTUAL, property_definitions);
+
+    if (parser && !IS_VIRTUAL.is) {
+        if (body_string == "unset" || body_string == "inherit" || body_string == "initial")
+            return new CSSProperty(rule_name, body_string, [new CSS_String(body_string)], important, pos);
+
 
         prop = parser.parse(wind(body_string));
 
-    else
+    } else
         //Need to know what properties have not been defined
         console.warn(`Unable to get parser for CSS property ${rule_name}`);
 
@@ -51,4 +55,8 @@ export default function parseDeclaration(sym, a, b, pos) {
         return new CSSProperty(rule_name, body_string, prop, important, pos);
 
     return null;
+}
+
+export function parseProperty(name: string, value: string, important: boolean): CSSProperty {
+    return parsePropertyDefinitionFromHydrocarbon([name, , value, important], undefined, undefined, undefined);
 }
