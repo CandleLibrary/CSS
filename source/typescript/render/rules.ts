30eb08b70970e72466fb280b11b3684a6cc7ffa1
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-09-12 17:56:13 -0600

    Modify render rule for media queries

--- source/typescript/render/rules.ts
+++ source/typescript/render/rules.ts
@@ -66,7 +66,7 @@ export const CSSNodeDefinitions = [
     },
     {
         type: CSSNodeType.MediaQueries,
-        template_pattern: "@... ",
+        template_pattern: "@...,",
     },
     {
         type: CSSNodeType.Media,
