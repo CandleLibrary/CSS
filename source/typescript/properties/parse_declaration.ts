97414b92a49fd4a1af6c126460ef14b767e0b702
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-09-12 17:57:01 -0600

    Modify parse_declaration.ts to gracefully handle invalid properties

--- source/typescript/properties/parse_declaration.ts
+++ source/typescript/properties/parse_declaration.ts
@@ -36,7 +36,6 @@ export default function parsePropertyDefinitionFromHydrocarbon(sym: { 0: string,
         important = sym[3] ? true : false,
         IS_VIRTUAL = { is: false };
 
-
     const parser = getPropertyParser(rule_name.replace(/\-/g, "_"), IS_VIRTUAL, property_definitions);
 
     if (parser && !IS_VIRTUAL.is) {
@@ -45,16 +44,16 @@ export default function parsePropertyDefinitionFromHydrocarbon(sym: { 0: string,
 
 
         prop = parser.parse(wind(body_string));
+        return new CSSProperty(rule_name, body_string, prop, important, pos);
+    } else {
 
-    } else
         //Need to know what properties have not been defined
         console.warn(`Unable to get parser for CSS property ${rule_name}`);
+        const prop = new CSSProperty(rule_name, body_string, null, important, pos);
+        prop.VALID = false;
+        return prop;
 
-
-    if (prop)
-        return new CSSProperty(rule_name, body_string, prop, important, pos);
-
-    return null;
+    }
 }
 
 export function parseProperty(name: string, value: string, important: boolean): CSSProperty {
