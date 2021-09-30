a7e1cbdc16e1d1510fcfeb314959f5f8476de634
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-09-30 12:56:10 -0600

    Chore: Update test

--- test/candlefw.css.spec.js
+++ test/candlefw.css.spec.js
@@ -1,82 +1,47 @@
-import {
-    renderWithFormatting,
-    rule,
-    selector,
-    parse,
-    isSelectorEqual,
-    doesRuleHaveMatchingSelector
-} from "@candlelib/css";
-
 import spark from "@candlelib/spark";
-
-"@candlelib//css test spec";
-"PARSER"; "#";
-
-const stylesheet = parse(
-    `div{ top:320%;
-    justify-content:space-around;
-    font-family:Arial, "Times new Roman";
-    box-shadow:inset 2px 2px 2px rgb(20,20,20);
-}
-@media screen and (min-width:900px) {
-    .sdfsf  #nav{padding: 0 2px;
-        z-index:-5820;
-    }
-}
-@media screen and ( max-width: 800px ) {
-    body{background-color:#ff0000}
-}`);
-const
-    result =
-        `div{top:320%;
-justify-content:space-around;
-font-family:Arial , "Times new Roman";
-box-shadow:inset 2px}
-
-
-    @media screen and (min-width:900px){.sdfsf #nav{padding:0 2px;
-z-index:-5820}}
-
-
-    @media screen and (max-width:800px){body{background-color:#ff0000}}`;
-
-const s = stylesheet.toString();
-
-"Expect parser-renderWithFormatting to match source string";
+import {
+    rule,
+    parse,
+    selector,
+} from "../build/library/parser/parse.js";
+import { renderCompressed } from "../build/library/render/render.js";
+import { isSelectorEqual, doesRuleHaveMatchingSelector } from "../build/library/selector/utilities.js";
 
 await spark.sleep(100);
 
-assert(renderWithFormatting(rule(".div{color: rgb(22, 22, 22)}")) == ".div{color:#161616}");
+const test = selector("svg|* a:nth-child(2n+1)");
 
-assert(renderWithFormatting(rule(".div {margin:2px 2px 2px;}")) == ".div{margin:2px 2px 2px}");
+assert(renderCompressed(rule(".div{color: rgb(22, 22, 22)}")) == ".div{color:#161616}");
 
-assert(renderWithFormatting(rule(".div{border: 2px solid green}")) == ".div{border:2px solid #008000}");
+assert(renderCompressed(rule(".div {margin:2px 2px 2px;}")) == ".div{margin:2px 2px 2px}");
 
-assert(renderWithFormatting(selector(".div")) == ".div");
+assert(renderCompressed(rule(".div{border: 2px solid green}")) == ".div{border:2px solid #008000}");
 
-assert(renderWithFormatting(selector("#div")) == "#div");
+assert(renderCompressed(selector(".div")) == ".div");
 
-assert(renderWithFormatting(selector("#div[src]")) == "#div[src]");
+assert(renderCompressed(selector("#div")) == "#div");
 
-assert(renderWithFormatting(selector("#div[src].a")) == "#div[src].a");
+assert(renderCompressed(selector("#div[src]")) == "#div[src]");
 
-assert(renderWithFormatting(selector(`#div[src ^= "true"].a`)) == `#div[src ^= "true"].a`);
+assert(renderCompressed(selector("#div[src].a")) == "#div[src].a");
 
-assert(renderWithFormatting(selector(`#div[src ^= "true" i].a`)) == `#div[src ^= "true" i].a`);
+assert(renderCompressed(selector(`#div[src ^= "true"].a`)) == `#div[src^=true].a`);
 
-assert(renderWithFormatting(selector("#div a")) == "#div a");
+assert(renderCompressed(selector(`#div[src ^= "true enough" i].a`)) == `#div[src^="true enough"i].a`);
 
-assert(renderWithFormatting(selector("svg|* a")) == "svg|* a");
+assert(renderCompressed(selector("#div a")) == "#div a");
 
-assert(renderWithFormatting(selector("svg|* a || a")) == "svg|* a || a");
+assert(renderCompressed(selector("svg|* a")) == "svg|* a");
 
-assert(renderWithFormatting(selector("svg|* a + a")) == "svg|* a + a");
+assert(renderCompressed(selector("svg|* a || a")) == "svg|* a || a");
 
-assert(renderWithFormatting(selector("svg|* a:test")) == "svg|* a:test");
+assert(renderCompressed(selector("svg|* a + a")) == "svg|* a + a");
 
-assert(renderWithFormatting(selector("svg|* a:nth-child(2n+1)")) == "svg|* a:nth-child(2n+1)");
+assert(renderCompressed(selector("svg|* a:test")) == "svg|* a:test");
 
-assert(renderWithFormatting(selector("svg|* a::after")) == "svg|* a::after");
+assert(renderCompressed(selector("svg|* a:nth-child(2n+1)")) == "svg|* a:nth-child(2n+1)");
+
+assert(renderCompressed(selector("svg|* a::after")) == "svg|* a::after");
 
 let a = selector("a[panda] .d"),
     b = selector("a[panda] .d");
